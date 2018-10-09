2da580ba10969a5b9de6466059e02129946d2157
saket <saket.shrivastava@digivalet.com>
2018-10-09 14:31:54 +0530

    working on offers detailed ui

---
+++ app/src/main/java/com/ehg/offers/OffersDetailActivity.java
@@ -0,0 +1,34 @@
+/*
+ *  Created by Emaar Hospitality Group on 9/10/18 12:31 PM
+ *  Copyright (C) 2018  All rights reserved.
+ *  Last modified 9/10/18 12:31 PM
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+package com.ehg.offers;
+
+import android.os.Bundle;
+import com.ehg.R;
+import com.ehg.home.BaseActivity;
+
+public class OffersDetailActivity extends BaseActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    setContentView(R.layout.activity_offersdetail);
+  }
+}
